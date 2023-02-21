c5568c5cc317ab85120ca3d5a2274e1e6991b601
Pavland <pvmacman@gmail.com>
2023-02-21 10:28:32 +0000

    Starting to sort data

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
 
+#reading test file
+data_initial = pd.read_csv('./files/train.csv')
+#Creating a dataset without Id column
+data = data_initial.drop(['Id'],axis=1)
 
-data = pd.read_csv('./files/test.csv')
-print(data)
+#sort by price
+print(data.sort_values(by=['SalePrice'],ascending=False))
+
+#looking for number of missing values in all columns
+#print(data.isnull().sum().sort_values(ascending=False).head(27))
